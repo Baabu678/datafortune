b3b089e213b9dab25ff0d73e6a5ef0afdf9a29ce
Babu <baabu@192.168.1.16>
2025-03-26 14:43:49 +0530

    confirmation screen is updated

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './confirmation.component.html',
   styleUrl: './confirmation.component.scss'
 })
-export class ConfirmationComponent {
+export class ConfirmationComponent implements OnInit {
+  constructor(private router : Router) { }
+
+  ngOnInit() {
+  }
+  
+goToLogin(): void {
+  this.router.navigate(['/login']);
+}
 
 }
